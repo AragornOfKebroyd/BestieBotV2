c4778538fa16eda6cc16850236dc43860ef42f0a
AragornOfKebroyd <benjamin.tinsley54@gmail.com>
2022-12-28 17:21:26 +0000

    e

--- events/xreply.js
+++ events/xreply.js
@@ -1,11 +1,17 @@
+const { Events } = require('discord.js');
+
 module.exports = {
-	name: 'messageCreate',
+	name: 'xreply',
 	execute(message) {
+		
 		console.log(`message sent ${message.content}`);
 	},
 };
 
 
 function determinex(userMessage){
-	return
+	index = 0
+	for (index = 0; index < userMessage.length-1; index++){
+
+	}
 }
